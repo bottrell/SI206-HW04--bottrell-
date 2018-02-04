69e81c0f4eae8a8e29f1740d83c8e40f4ea6797a
emimeirl <emimeirl@umich.edu>
2018-02-04 10:07:44 -0500

    1st B finished

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,17 @@
+import random
+
 def user_input():
 	question = input("What is your question?")
 
+def pick_answer():
+	 answers=["It is certain", "It is decidedly so", "Without a doubt",
+	"Yes definitely", "You may rely on it", "As I see it, yes", "Most likely",
+	"Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+	"Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+	"Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+	picked=random.choice(answers)
+
+	return picked
 if __name__ == "__main__":
 	main()
